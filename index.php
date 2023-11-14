8701916af72df70655dba69e6a3fb89940e11540
walysion <89873423+walysion@users.noreply.github.com>
2023-11-14 15:38:35 -0300

    index.php

--- index.php
+++ index.php
@@ -1,28 +1,28 @@
 <?php
 
-include '/includePHP/head.php';
+include 'includePHP\head.php';
 
 ?>
 
 
 <?php
 
-include '/includePHP/giratorio.php';
-include '/includePHP/barrasuperior.php';
-include '/includePHP/navbar.php';
-include '/includePHP/carrusel.php';
-include '/includePHP/acercade.php';
-include '/includePHP/iniciodeloshechos.php';
-include '/includePHP/caracteristicasdeinicio.php';
-include '/includePHP/videmodal.php';
-include '/includePHP/servicios.php';
-include '/includePHP/proyectos.php';
-include '/includePHP/team.php';
-include '/includePHP/socios.php';
-include '/includePHP/footer.php';
-include '/includePHP/copyright.php';
-include '/includePHP/volverarriba.php';
-include '/includePHP/libreriajavascript.php';
+include 'includePHP\giratorio.php';
+include 'includePHP\barrasuperior.php';
+include 'includePHP\navbar.php';
+include 'includePHP\carrusel.php';
+include 'includePHP\acercade.php';
+include 'includePHP\iniciodeloshechos.php';
+include 'includePHP\caracteristicasdeinicio.php';
+include 'includePHP\videmodal.php';
+include 'includePHP\servicios.php';
+include 'includePHP\proyectos.php';
+include 'includePHP\team.php';
+include 'includePHP\socios.php';
+include 'includePHP\footer.php';
+include 'includePHP\copyright.php';
+include 'includePHP\volverarriba.php';
+include 'includePHP\libreriajavascript.php';
 
 
 
