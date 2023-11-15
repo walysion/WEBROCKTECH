aee44b12191254cc6d89539c93456d500ef0343e
walterrocktechticket <estampcolorchile@gmail.com>
2023-11-15 17:59:03 -0300

    cambiosinlclude

--- controldeacceso.php
+++ controldeacceso.php
@@ -37,9 +37,9 @@
  
 
     <?php
-    include 'includePHP\giratorio.php';
-    include 'includePHP\barrasuperior.php';
-    include 'includePHP\navbar.php';
+    include '/var/www/html/includePHP/giratorio.php';
+    include '/var/www/html/includePHP/barrasuperior.php';
+    include '/var/www/html/includePHP/navbar.php';
     
     
     ?>
@@ -238,9 +238,9 @@
     <?php
 
 
-      include 'includePHP\libreriajavascript.php';
-      include 'includePHP\footer.php';
-      include 'includePHP\copyright.php';
+      include '/var/www/html/includePHP/libreriajavascript.php';
+      include '/var/www/html/includePHP/footer.php';
+      include '/var/www/html/includePHP/copyright.php';
     
     ?>
 
